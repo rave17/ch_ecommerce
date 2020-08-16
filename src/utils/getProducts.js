fccc646476e99a8c1664c831a38fbd0c729b95be
roberto <rave17491@gmail.com>
2020-08-16 00:54:01 -0300

    useful functions

---
+++ src/utils/getProducts.js
@@ -0,0 +1,43 @@
+export default function getProducts() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res([{
+                id: "01",
+                name: "remera",
+                description: "una remera",
+                price: 220
+            },
+            {
+                id: "02",
+                name: "pantalon",
+                description: "un pantalon",
+                price: 550
+            },
+            {
+                id: "03",
+                name: "pollera",
+                description: "una pollera",
+                price: 340
+            },
+            {
+                id: "04",
+                name: "camisa",
+                description: "una camisa",
+                price: 280
+            },
+            {
+                id: "05",
+                name: "campera",
+                description: "una campera",
+                price: 700
+            },
+            {
+                id: "06",
+                name: "zapatilla",
+                description: "unas zapatillas",
+                price: 800
+            }])
+        }, )
+        console.log(res)
+    });
+}
---
+++ src/utils/login.js
@@ -0,0 +1,10 @@
+export default function loginUser(user, pass){
+    
+    console.log('ejemplo')
+    if( user & pass ){
+        console.log(`user: ${user} / password: ${pass}`);
+    }
+    return true;
+
+}
+
